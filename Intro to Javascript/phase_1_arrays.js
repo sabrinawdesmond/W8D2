4697c8ccc27e577f12025f25f08ad470b191e326
Milner Chen <milnerchen0@gmail.com>
2023-02-28 15:45:29 -0500

    phase 1

--- Intro to Javascript/phase_1_arrays.js
+++ Intro to Javascript/phase_1_arrays.js
@@ -12,25 +12,47 @@ Array.prototype.uniq = function() {
 // console.log([1,2,2,3,4,3,2].uniq())
 
 Array.prototype.twoSum = function() {
-  final = []
+  let final = [];
   for (let i = 0; i < this.length - 1; i++) {
     for (let j = i + 1; j < this.length; j++) {
-      num1 = this[i]
-      num2 = this[j]
+      num1 = this[i];
+      num2 = this[j];
       if (num1 + num2 === 0) {
-        final.push([i, j])
+        final.push([i, j]);
       }
     }
   }
-  return final
+  return final;
 }
 
 // console.log([1,-1,2,3,-2,4].twoSum())
 
-Array.prototype.transpose = funtion() {
-  let final = []
-
+Array.prototype.transpose = function() {
+  let final = [];
   for (let i = 0; i < this[0].length; i++) {
-    
+    let temp = []
+    debugger;
+    for (let j = 0; j < this.length; j++) {
+      debugger;
+      temp.push(this[j][i]);
+    }
+    final.push(temp)
   }
+  return final;
 }
+
+console.log([
+  [1, 2, 3],
+  [4, 5, 6]
+].transpose())
+
+// final[i].push(j)
+// [1, 2, 3]
+// [4, 5, 6]
+
+// [0][0] = 1
+// [1][0] = 4
+
+// [1, 4]
+// [2, 5]
+// [3, 6]
